2ee846c898af1a1f1382bf7c130293020c02af56
James W <james@jamesrwilliams.co.uk>
2018-03-07 17:33:25 +0000

    Started lib

---
+++ src/qs-forms.js
@@ -0,0 +1,23 @@
+"use strict";
+
+window.qs = (function(){
+	
+	function QS(){
+		
+		console.log('init');
+		
+	}
+	
+	var qs = {
+		
+		sayHello: function(){
+			
+			console.log("Hello");
+			
+		}
+		
+	}
+	
+	return qs;
+	
+}());
