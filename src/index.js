92a6df87601392f00a2a264703b155c02ffcfb43
James Williams <james@jamesrwilliams.co.uk>
2018-03-07 21:33:49 +0000

    refactor: Moved to Webpack & bable

---
+++ src/index.js
@@ -0,0 +1,13 @@
+var QS = class {
+
+  constructor() {
+
+    console.log('Fired');
+
+    this._name = 'Hello World';
+
+  }
+
+};
+
+export { QS };
---
+++ test/index.spec.js
@@ -0,0 +1,15 @@
+/* global describe, it, before */
+
+import chai from 'chai';
+
+var assert	= require('assert');
+
+describe('qs', function(){
+
+	describe('#init()', function() {
+		it("initialises correctly", function(){
+			assert.equal(true, true);
+		});
+	});
+
+});
