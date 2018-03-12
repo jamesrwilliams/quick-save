a57d3e74dd7fcc039560de83c08a565ff4701380
James Williams <james@jamesrwilliams.co.uk>
2018-03-12 07:48:31 +0000

    feat: Basic DOM traversal and updated demo

--- src/index.js
+++ src/index.js
@@ -4,7 +4,39 @@ export default class qs {
 
     console.log('QS Demo');
 
-    this.property = true;
+    this.$elms = document.querySelectorAll('[data-qs]');
+
+    for (let i = 0; i < this.$elms.length; i++) {
+
+      let $elm = this.$elms[i];
+
+      if (!$elm.name) {
+
+        console.log("QS - Form Elements need a 'name' attribute.");
+
+      }
+
+      window.localStorage.setItem($elm.name, JSON.stringify($elm.value));
+
+      // $elm.addEventListener('input', function(event){
+
+      // console.log("Changed", event);
+
+      // });
+
+    }
+
+  }
+
+  writeLocalStorage($elm) {
+
+    console.log('writeLocalStorage');
+
+  }
+
+  readLocalStorage($elm) {
+
+    console.log('readLocalStorage');
 
   }
 
